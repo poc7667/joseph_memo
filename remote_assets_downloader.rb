bdace3aa3110831536934075d8f586ac798c95f6
poc7667 <poc7667@gmail.com>
2014-07-29 15:55:40 +0800

    Update
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- remote_assets_downloader.rb
+++ remote_assets_downloader.rb
@@ -8,28 +8,36 @@ require 'pry-nav'
 require 'pathname'
 require 'net/http'
 require 'fileutils'
+
 Pry.config.editor = "vim"
 
 def get_basename(f_path)
-  Pathname.new(f_path).basename.to_s
-end
-
-def template(url)
-  if url.include? 'js'
-    "<script src=\"js/#{get_basename(url)}\" type=\"text/javascript\"></script>"
-  elsif url.include? 'css'
-    "<link href=\"css/#{get_basename(url)}\" media=\"all\" rel=\"stylesheet\" type=\"text/css\"></link>"
+  raw_fpath = Pathname.new(f_path).basename.to_s
+  m = raw_fpath.scan(/(.*)(\?)/)
+  if m.count >0
+    return m[0][0]
+  else
+    return raw_fpath
   end
 end
+
+def get_asset_fpath(url)
+  if url.include? '.js'
+    "js/#{get_basename(url)}" 
+  elsif url.include? '.css'
+    "css/#{get_basename(url)}"      
+  end
+end
+
 
 class HttpDownloader
   def initialize(resource_type, url)
     FileUtils::mkdir_p resource_type
-    download(url, resource_type)
+    download(url, resource_type)    
   end
   def download(url, resource_type)
     Dir.chdir(resource_type)
-    system("wget #{url} > /dev/null" ) unless File.file?(get_basename(url))
+    system("curl #{url} -o #{get_basename(url)}" ) unless File.file?(get_basename(url))
     Dir.chdir('../')
   end
 end
@@ -40,7 +48,7 @@ class RemoteAssetsDownloader
     @assets = {js: [], css: []}
     get_assets
     do_download
-    ap(@assets)
+    export_html
   end
 
   def export_html
@@ -50,32 +58,34 @@ class RemoteAssetsDownloader
   end
   
   def do_download
-    f = open('html.txt', 'w')
     @assets.each do |resource_type, values|
       values.each do |url, template|
         HttpDownloader.new(resource_type.to_s, url)
-        f.puts template
       end
     end
-    f.close
   end
 
   def get_assets
-    ['link','script'].each do |assets_type|
+    [['link', 'href', 'css'],['script', 'src', 'js']].each do |assets_type, node_attr, ext_name|
       @page.css(assets_type).each do |node|
         begin
-          if assets_type=='link'
-            url_path = node.attributes["href"].value 
-            @assets[:css] << [url_path, template(url_path)] if url_path.include? ".css"
-          elsif assets_type=='script'
-            url_path = node.attributes["src"].value
-            @assets[:js] << [url_path, template(url_path)] if url_path.include? ".js"
+          url_path = node.attributes[node_attr].value          
+          if url_path.include? ".#{ext_name}" and url_path.include? "http" # if url contains .css or .js
+            @assets[ext_name.to_sym] << [url_path]             
+            if get_asset_fpath(url_path)
+              node.attributes[node_attr].value = get_asset_fpath(url_path).strip
+              ap(node.attributes[node_attr])
+            else
+              binding.pry
+            end
           end
         rescue Exception => e
+          p node.attributes[node_attr]
+          p e
         end
       end
     end
-  end
+  end # end get_assets
 end
 
 dl = RemoteAssetsDownloader.new(ARGV[0])
